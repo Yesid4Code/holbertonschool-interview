6283c8d6c17879ea45f0afce4ad73efd67965db1
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-01-06 00:39:51 -0500

    PEP8 codestyle

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 '''
-This file contain a function that calculate the fewest number of operation needed
-to copy a characer
+This file contain a function that calculate the fewest number of operation
+needed to copy a characer
 '''
 
+
 def minOperations(n):
-        '''
-        Function that calculate the fewest number of operation needed
-        to copy a characer 'n' times.
-        Args:
-                n: integer, number of final characters desired.
-        Return:
-                integer that represent the number of operations to achive the solutions.
-                0 if n  is imposible to achive.
-        '''
-        if (type(n) != int or n <= 1):
-                return 0
+    '''
+    Function that calculate the fewest number of operation needed
+    to copy a characer 'n' times.
+    Args:
+        n: integer, number of final characters desired.
+    Return:
+        integer that represent the number of operations to achive the solutions
+        0 if n  is imposible to achive.
+    '''
+    if (type(n) != int or n <= 1):
+        return 0
 
-        operations = 0
-        i = 2
-        while (i <= n):
-                while (n % i == 0):
-                        operations += 1
-                        n /= i
-                i += 1
-        return operations
-
+    operations = 0
+    i = 2
+    while (i <= n):
+        while (n % i == 0):
+            operations += 1
+            n /= i
+        i += 1
+    return operations
