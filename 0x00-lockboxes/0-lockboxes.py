f954ffc89f3488c0379ccf368e22b1c6e19390e4
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-01-06 04:08:39 -0500

    LockBoxes method

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''''
+This file contain a function to check if boxes can be opened.
+'''
+
+
+def searchkey(boxes, idx):
+    i = 0
+    keys = []
+    for box in boxes:
+        if i == idx:
+            i += 1
+            continue
+        for key in box:
+            if key not in keys:
+                keys.append(key)
+        i += 1
+    return keys
+
+
+def canUnlockAll(boxes):
+    """
+    Method that determines if all boxes can be openend.
+    Args:
+        boxes: list of list. The content of the lists are the keys.
+    Return:
+        True if all boxes can be openen, False otherwise.
+    """
+    index_keys = {}
+    idx = 0
+
+    for box in boxes:
+        if idx != 0:
+            index_keys[idx] = searchkey(boxes, idx)
+        idx += 1
+
+    i = 0
+    for key, values in index_keys.items():
+        if key not in values:
+            return False
+        i += 1
+    return True
