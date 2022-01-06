14aedabe187c8af8742746dab5e399902d7bc3fd
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-01-06 05:35:23 -0500

    delete the dictionary

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,18 +5,14 @@ This file contain a function to check if boxes can be opened.
 
 
 def searchkey(boxes, idx):
-    """keys that could open the box"""
+    """Found the key of a box"""
     i = 0
-    keys = []
+
     for box in boxes:
-        if i == idx:
-            i += 1
-            continue
-        for key in box:
-            if key not in keys:
-                keys.append(key)
+        if i != idx and idx in box:
+            return True
         i += 1
-    return keys
+    return False
 
 
 def canUnlockAll(boxes):
@@ -27,13 +23,8 @@ def canUnlockAll(boxes):
     Return:
         True if all boxes can be openen, False otherwise.
     """
-    index_keys = {0: [0]}
 
     for idx in range(len(boxes)):
-        if idx != 0:
-            index_keys[idx] = searchkey(boxes, idx)
-
-    for key, values in index_keys.items():
-        if key not in values:
+        if idx != 0 and not searchkey(boxes, idx):
             return False
     return True
