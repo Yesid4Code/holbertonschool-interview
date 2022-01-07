045d2e9960c8def325f78a5f484ddf9c69038479
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2022-01-07 00:35:49 -0500

    gunction to create a new node inside a binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node -  function that creates a binary tree node.
+ * @parent: is a pointer to the parent node of the node to create.
+ * @value: is the value to put in the new node.
+ *
+ * Return: number of nodes
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,17 @@
+# Heap Insert
+
+| **Filename** | **Description** |
+|---|---|
+| [0. New node](./0-binary_tree_node.c/) | function that creates a binary tree node. |
+| [1. Heap - Insert](./1-heap_insert.c/) |  function that inserts a value into a Max Binary Heap. |
+
+
+# output
+```c
+				.----------------------(006)-------.
+			  .--(001)-------.                   .--(008)--.
+		    .--(-02)       .--(003)-------.       (007)     (009)
+	 .-------(-06)          (002)       .--(005)
+   .--(-08)--.                           (004)
+(-09)     (-07)
+```
